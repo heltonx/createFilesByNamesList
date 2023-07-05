613b85981ee2520d8dd4541379536967c442dad0
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2023-07-05 16:24:04 -0300

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+
+#Replace here by the file you want copies with different names
+FILE_TO_REPLICATE = 'L00000000000000.pdf'
+
+isExist = os.path.exists('result/') #if result directory does not exist, creates it
+if not isExist:
+	os.makedirs('result/')
+
+arrayNames = [line.rstrip() for line in open('sampleList.txt')]
+
+sumNames = len(arrayNames)
+
+for i in range (sumNames):
+	joiner ='result/'+arrayNames[i]+'.pdf'
+	shutil.copyfile(FILE_TO_REPLICATE,joiner)
